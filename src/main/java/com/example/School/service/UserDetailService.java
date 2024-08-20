b0e787c2597fffe57caca371b3ad74eb64fd2d90
Rohan <rohanpai126@gmail.com>
2024-08-20 18:46:51 +0530

    rohan

--- src/main/java/com/example/School/service/UserDetailService.java
+++ src/main/java/com/example/School/service/UserDetailService.java
@@ -1,39 +1,37 @@
 package com.example.School.service;
-
 import com.example.School.dto.UserDetailDTO;
 import com.example.School.repository.UserDetailRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
-
 @Slf4j
 @Service
 public class UserDetailService {
     @Autowired
     UserDetailRepository userDetailRepository;
-
     public void addUserDetail(UserDetailDTO userDetailDTOList) {
-
+        try {
             System.out.println("ID:"+userDetailDTOList.getId());
             System.out.println("Address:"+userDetailDTOList.getAddress());
             System.out.println("Father:"+userDetailDTOList.getFather());
             System.out.println("mother:"+userDetailDTOList.getMother());
             userDetailRepository.addUserDetail(userDetailDTOList.getId(),userDetailDTOList.getAddress(),userDetailDTOList.getFather(),userDetailDTOList.getMother());
+        } catch (Exception ex) {
+            ex.printStackTrace();
 
 
+        }
     }
-
     public void addUserDetail2(List<UserDetailDTO> userDettailDTOList){
-
+        try {
             for (UserDetailDTO userDetailDTO : userDettailDTOList) {
                 userDetailRepository.addUserDetail(userDetailDTO.getId(),userDetailDTO.getAddress(), userDetailDTO.getFather(),userDetailDTO.getMother());
             }
-
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
-
     public List<UserDetailDTO> getAllUserDetail() {
         try {
             List<UserDetailDTO> response = userDetailRepository.getAllUserDetail();
@@ -43,7 +41,6 @@ public class UserDetailService {
         }
         return null;
     }
-
     public UserDetailDTO getUserDetailById(String id) {
         try {
             UserDetailDTO response = userDetailRepository.getUserDetailById(id);
@@ -54,16 +51,13 @@ public class UserDetailService {
         }
         return null;
     }
-
     public void updateUserDetail(String id, String address){
         try {
             userDetailRepository.updateUserDetail(id, address);
         }catch (Exception ex){
             System.out.println("Exception occured ||" + ex.getMessage());
         }
-
     }
-
     public void deleteUserDetail(String id){
         try {
             userDetailRepository.deleteUserDetail(id);
@@ -72,4 +66,3 @@ public class UserDetailService {
         }
     }
 }
-
